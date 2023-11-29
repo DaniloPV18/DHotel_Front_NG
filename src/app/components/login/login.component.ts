c8942ce46f265cbbe49b6c6b0f3c1aa1692b57bb
DaniloPV18 <damipive1809@gmail.com>
2023-11-29 11:30:45 -0500

    Dependencias y estilos agregadas para el login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+
+export class LoginComponent implements OnInit {
+  loginFormGroup!: FormGroup;
+  submitted: boolean = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.loginFormGroup = new FormGroup({
+      email: new FormControl("", Validators.compose([Validators.email, Validators.required])),
+      contrasena: new FormControl("", Validators.required),
+    });
+  }
+
+  onSubmit() {
+
+  }
+
+  get loginFormGroupControl() {
+    return this.loginFormGroup.controls;
+  }
+}
