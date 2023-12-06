6cc938fc7c77893a9289c663dae8ec42d5d393e6
DaniloPV18 <damipive1809@gmail.com>
2023-12-06 10:33:04 -0500

    Modal delete agregado - En proceso

---
+++ src/app/components/options-main/users/users-delete/users-delete.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-users-delete',
+  templateUrl: './users-delete.component.html',
+  styleUrl: './users-delete.component.css'
+})
+export class UsersDeleteComponent {
+  constructor(
+    private _dialogRef: MatDialogRef<UsersDeleteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    console.log(data.dataText);
+  }
+
+  onSubmit() {
+
+  }
+
+  cancel() {
+    this._dialogRef.close();
+  }
+}
