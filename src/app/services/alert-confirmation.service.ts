5cde04c08c3a616960dedf800bb4c382ee860f66
DaniloPV18 <damipive1809@gmail.com>
2024-01-12 21:35:06 -0500

    Sweet alert | Installed

---
+++ src/app/services/alert-confirmation.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertConfirmationService {
+
+  constructor() { }
+
+  showSuccessAlert(text: string, i: number): Promise<any> {
+    switch (i) {
+      case 1:
+        return Swal.fire({
+          title: '¡Éxito!',
+          text: text,
+          icon: 'success',
+          confirmButtonText: 'Ok',
+          customClass: {
+            container: 'custom-swal'
+          }
+        });
+      case 2:
+        return Swal.fire({
+          title: 'Error!',
+          text: text,
+          icon: 'error',
+          confirmButtonText: 'Ok',
+          customClass: {
+            container: 'custom-swal'
+          }
+        });
+      default:
+        return Promise.reject('Opción no válida');
+    }
+  }
+}
